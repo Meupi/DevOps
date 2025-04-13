46b271afb2d1d98e2a76df709992467fb5086b9e
Meupi <marco.leupi1@gmail.com>
2025-04-13 23:10:47 +0200

    added word status diplayer function

--- Hangman.py
+++ Hangman.py
@@ -4,6 +4,15 @@ secret_word = input("Player 1: Choose a secret word:")
 # letter input function
 letter = input("Player 2: Choose a letter:")
 
+# word status displayer
+def word_status():
+    for elem in secret_word.upper():
+        if elem != letter:
+            print("_", end=" ")
+        else:
+            print(letter, end=" ")
+
+word_status()
 
 
 
