6b238474c56ed62cecbed29fd6511704793d2dfb
Meupi <marco.leupi1@gmail.com>
2025-04-14 22:48:01 +0200

    first try: hangman function

--- Hangman.py
+++ Hangman.py
@@ -8,7 +8,7 @@ n_tries = 0
 letter = input("Player 2: Choose a letter:")
 
 # word status displayer
-def word_status():
+def word_status(secret_word, letter):
     for elem in secret_word.upper():
         if elem != letter:
             print("_", end=" ")
@@ -16,14 +16,31 @@ def word_status():
             print(letter, end=" ")
 
 # failed attempts function
-def failed_attempts():
-    global n_tries
+def failed_attempts(secret_word, letter, n_tries):
     if letter not in secret_word:
         n_tries += 1
         print(str(n_tries) + " out of 5 tries.")
     elif n_tries == 5:
         print(str(n_tries) + " out of 5 tries. You lose!")
         return False
+
+# letter list function
+def letter_list(letter):
+    letter_list = []
+    if letter not in letter_list:
+        letter_list.append(letter)
+    else:
+        print(letter + "has already been chosen. Choose another letter!")
+# the game
+def hangman():
+    n_tries = 0
+    secret_word = input("Player 1: Choose a secret word:")
+    while True:
+        letter = input("Player 2: Choose a letter:")
+        word_status(secret_word, letter)
+        failed_attempts(secret_word, letter, n_tries)
+
+hangman()
 
 
 
