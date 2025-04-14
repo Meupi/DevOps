8034893563191234c03548ebb431d305ff48294c
Meupi <marco.leupi1@gmail.com>
2025-04-14 21:32:52 +0200

    added global n_tries var

--- Hangman.py
+++ Hangman.py
@@ -1,14 +1,21 @@
 # secret word input function
 secret_word = input("Player 1: Choose a secret word:")
 
+# try counter
+n_tries = 0
+
 # letter input function
 letter = input("Player 2: Choose a letter:")
 
 # word status displayer
 def word_status():
+    global n_tries
     for elem in secret_word.upper():
         if elem != letter:
             print("_", end=" ")
+            print(str(n_tries) + "out of 5 tries.")
+            n_tries += 1
+
         else:
             print(letter, end=" ")
 
