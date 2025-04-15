d2ebb8bd218a0f149eb4904cb8c294a83941b616
Meupi <marco.leupi1@gmail.com>
2025-04-15 23:18:21 +0200

    failed_attempts function count now the number of tries

--- Hangman.py
+++ Hangman.py
@@ -10,13 +10,6 @@ n_tries = 0
 letter = input("Player 2: Choose a letter:")
 
 # word status displayer
-def word_status(secret_word, letter):
-    for elem in secret_word.upper():
-        if elem != letter:
-            print("_", end=" ")
-        else:
-            print(letter, end=" ")
-
 def word_status(secret_word, letter, try_word):
     position = [index for index,char in enumerate(secret_word) if char == letter]
     try_word_ls = list(try_word)
@@ -35,10 +28,10 @@ def set_try_word(secret_word):
     return try_word
 
 # failed attempts function
-def failed_attempts(secret_word, letter, n_tries):
+def failed_attempts(secret_word, letter):
     if letter not in secret_word:
-        n_tries += 1
         global n_tries
+        n_tries += 1
         print(str(n_tries) + " out of 5 tries.")
     elif n_tries == 5:
         print(str(n_tries) + " out of 5 tries. You lose!")
@@ -59,7 +52,7 @@ def hangman():
     while True:
         letter = input("Player 2: Choose a letter:")
         try_word = word_status(secret_word, letter, try_word)
-        failed_attempts(secret_word, letter, n_tries)
+        failed_attempts(secret_word, letter)
 
 hangman()
 
