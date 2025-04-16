274a79639954c57b2edfa3979c37e5e98d8cecd6
Meupi <marco.leupi1@gmail.com>
2025-04-16 22:47:28 +0200

    some bugfixing in order to receive the correct loose/win statement

--- Hangman.py
+++ Hangman.py
@@ -28,14 +28,12 @@ def set_try_word(secret_word):
     return try_word
 
 # failed attempts function
-def failed_attempts(secret_word, letter):
+def failed_attempts(secret_word, letter, n_tries):
     if letter not in secret_word:
-        global n_tries
         n_tries += 1
         print(str(n_tries) + " out of 5 tries.")
-        if n_tries == 5:
-            print(str(n_tries) + " out of 5 tries. You lose!")
-            return n_tries
+        return n_tries
+
 
 def loop_breaker(secret_word, try_word, n_tries):
     if secret_word != try_word and n_tries != 5:
@@ -47,24 +45,26 @@ def loop_breaker(secret_word, try_word, n_tries):
 
 # letter list function
 def letter_list(letter):
-    letter_list = []
     if letter not in letter_list:
         letter_list.append(letter)
     else:
         print(letter + "has already been chosen. Choose another letter!")
 
+def outcome(n_tries, secret_word, try_word):
+    if n_tries == 5 and secret_word != try_word:
+        print("You loose!")
+    elif secret_word == try_word:
+        print("Congrats! You won!")
 
 # the game
 def hangman():
     n_tries = 0
+    letter_list = []
     secret_word = input("Player 1: Choose a secret word:")
     try_word = set_try_word(secret_word)
     while loop_breaker(secret_word, try_word, n_tries):
         letter = input("Player 2: Choose a letter:")
         try_word = word_status(secret_word, letter, try_word)
-        n_tries = failed_attempts(secret_word, letter)
-
+        n_tries = failed_attempts(secret_word, letter, n_tries)
+    outcome(n_tries, secret_word, try_word)
 hangman()
-
-
-
